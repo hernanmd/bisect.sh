357e00f1ded84d028281d45bb936f5a859e7f0ea
Hernán Morales Durand <hernan.morales@gmail.com>
2024-01-24 17:26:43 +0100

    Update

--- bisect.sh
+++ bisect.sh
@@ -1,9 +1,39 @@
 #!/bin/bash
 
+# set -x
+# Fail fast and be aware of exit codes
+set -eo pipefail
+
+readonly ERR_INVALID_USAGE=2
+
 combined_xml_file_name="combined.xml"
 should_clean_xml_files=true
 pharo_image="Pharo.image"
 stage_name="Tests-osx-64"
+
+show_usage() {
+	cat <<-EOF
+		Usage: $(basename $0) COMMAND
+
+		Commands:
+			-h|--help       Show usage
+			-i|--image      Run image
+			-v|--version	Show version
+	EOF
+}
+
+usage() {
+	show_usage
+	exit $ERR_INVALID_USAGE
+}
+    
+die() {
+	local message="$1"
+	local -i exit_code="${2:-1}"
+
+	printf "%sError $exit_code: $message%s\n" "${RED}" "${NORMAL}" >&2
+	exit $exit_code
+}
 
 # Returns 0 if command was found in the current system, 1 otherwise
 cmd_exists () {
@@ -18,13 +48,13 @@ run_tests(){
 
 # Check if external programs are installed, or stop execution
 check_requirements() {
-    cmd_exists jrm || { printf "jrm is missing. Install with: npm install -g junit-report-merger\n"; exit 1; }
-    cmd_exists xpath || { printf "xpath is missing\n"; exit 1; }
+    cmd_exists jrm || { die "jrm is missing. Install with: npm install -g junit-report-merger\n"; }
+    cmd_exists xpath || { die "xpath is missing\n"; }
 }
 
 combine_xml_files() {
     jrm "$combined_xml_file_name" ./*.xml
-    [[ -f "$combined_xml_file_name" ]] || { printf "Could not combine XML files\n"; exit;}
+    [[ -f "$combined_xml_file_name" ]] || { die "Could not combine XML files\n"; }
     if [ $should_clean_xml_files == true ] ; then
         find . -name '*.xml' ! -name "$combined_xml_file_name" -exec rm \{\} +
     fi
@@ -38,8 +68,30 @@ filter_failures() {
     grep '>>.*test.*' "$failures_file_name"
 }
 
-main () {
+bisect() {
     check_requirements
     combine_xml_files
     filter_failures
 }
+
+main () {
+		[[ $# -gt 0 ]] || usage
+
+		local command="${1:-}"
+		case "$command" in
+			("-h" | "--help")
+				shift
+				show_usage
+				;;
+			("-i" | "--image")
+				shift
+				bisect "${@:2}"
+				;;
+			(*)
+				shift
+				show_usage
+				;;
+		esac
+}
+
+main "$@"
